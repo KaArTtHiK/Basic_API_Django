b9c59ed2c45ac0ed8caf41c9663f81b4d08ace01
KaArTtHiK <sirigirikarthik00@gmail.com>
2021-01-15 18:55:47 +0530

    basicapi application

---
+++ project3/basic_api_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BasicApiAppConfig(AppConfig):
+    name = 'basic_api_app'
---
+++ project3/basic_api_app/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path,include
+from .views import article_list,detail_article,Article_APIView,Detail_APIView,GenericAPIView, ArticleViewSet, GenericViewSet,ModelViewSet
+from rest_framework.routers import DefaultRouter
+from basic_api_app import views
+
+router = DefaultRouter()
+router.register('article', ModelViewSet, basename = 'article')
+urlpatterns = [
+    path('',views.index,name = "index"),
+    path('viewset/',include(router.urls)),
+    path('viewset/<int:pk>',include(router.urls)),
+    #path('article/',article_list ),
+    path('article/', Article_APIView.as_view()),
+    #path('detail/<int:pk>/',detail_article),
+    path('detail/<int:id>/',Detail_APIView.as_view()),
+    path('generic/article/<int:id>/',GenericAPIView.as_view())
+]
